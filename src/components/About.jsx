80eb09dadce12a4d8fa41388f2e1238c3f9394ac
AndresN <camilonunezz9812@gmail.com>
2021-08-06 21:35:56 -0500

    hooks implemented for typing effect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { aboutData } from '../data'
 import DevImg from '../images/AboutDeveloper.svg'
@@ -6,10 +6,26 @@ import './styles/About.css';
 
 export default function About (props) {
     const data = aboutData[props.language];
+    const [title, setTitle] = useState('');
+    const [counter, setCounter] = useState(0);
+    const speed = 120;
+    const titleData = data.title;
+    useEffect(() => {
+        setTimeout(() => {
+            if (counter < titleData.length) {
+                setCounter(counter => counter + 1);
+                setTitle(title + titleData.charAt(counter))
+            }
+        }, speed);
+    }, [title]);
+    useEffect(() => {
+        setTitle('');
+        setCounter(0);
+    }, [props.language]);
     return (
         <section id="about">
             <div className="About__text">
-                <h1 className="About__title">{data.title}</h1>
+                <h1 className="About__title">{title}</h1>
                 <p className="About__content">{data.content}</p>
             </div>
             <div className="About__image">
