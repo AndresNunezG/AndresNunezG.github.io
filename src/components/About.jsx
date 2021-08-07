b29cf11c1951bd17eb7514d4543e96a80110a45e
AndresN <camilonunezz9812@gmail.com>
2021-08-06 22:18:03 -0500

    Custom hook useTypeEffect created and implemented with no visible bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,35 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useTypeEffect from '../hooks/useTypeEffect';
 
 import { aboutData } from '../data'
 import DevImg from '../images/AboutDeveloper.svg'
 import './styles/About.css';
 
-const useTypeEffect = (text, speed) => {
-    const hola = '';
-};
-
 export default function About (props) {
     const data = aboutData[props.language];
-    const [title, setTitle] = useState('');
-    const [counter, setCounter] = useState(0);
     const speed = 120;
-    useEffect(() => {
-        setTimeout(() => {
-            if (counter < data.title.length) {
-                setCounter(counter => counter + 1);
-                setTitle(title + data.title.charAt(counter))
-            }
-        }, speed);
-    }, [title]); // eslint-disable-line react-hooks/exhaustive-deps
-    useEffect(() => {
-        setTitle('');
-        setCounter(0);
-    }, [props.language]);
+    const textEffect = useTypeEffect(data.title, speed, props.language)
     return (
         <section id="about">
             <div className="About__text">
                 <div className="AboutTitle__container">
-                    <h1 className="About__title">{title}</h1>
+                    <h1 className="About__title">{textEffect}</h1>
                 </div>
                 <p className="About__content">{data.content}</p>
             </div>
---
+++ src/hooks/useTypeEffect.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useTypeEffect = (text, speed, reset) => {
+    const [textEffect, setTextEffect] = useState('');
+    const [counter, setCounter] = useState(0);
+    useEffect(() => {
+        setTimeout(() => {
+            if (counter < text.length) {
+                setCounter(counter => counter + 1);
+                setTextEffect(textEffect + text.charAt(counter))
+            }
+        }, speed);
+    }, [textEffect]); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        setTextEffect('');
+        setCounter(0);
+    }, [reset]);
+    return (textEffect);
+};
+
+export default useTypeEffect;
